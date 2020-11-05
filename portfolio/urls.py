73cac803561f36fe7865e8dbafd15312fd97a8f8
tokuhisa <TokuhisaH1122>
2020-11-05 19:27:24 +0900

    complete home&profile and created other pages

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,9 +1,11 @@
 #app
 
 from django.conf.urls import url
-from .views import homefunc,profilefunc
+from .views import homefunc,activityfunc,photographyfunc,contactfunc
 
 urlpatterns = [
-    url('profile/', profilefunc,name='profile'),
+    url('activity/', activityfunc,name='activity'),
+    url('photography/', photographyfunc,name='photography'),
+    url('contact/', contactfunc,name='contact'),
     url('', homefunc,name='home'),
 ]
